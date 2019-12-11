ff223a7e4973edcebc260d56329b35f66cb8895e
Jamie <jamielaks@gmail.com>
2019-12-11 06:56:02 -0500

    Some test code for the controllers

---
+++ EGD_Project3/Assets/FullControllerTest.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Uduino;
+
+public class FullControllerTest : MonoBehaviour
+{
+    public int speed = 3;
+    public UduinoManager u;
+    //public bool isPaused;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //isPaused = false;
+        u = UduinoManager.Instance;
+        u.DiscoverPorts();
+        for (int i = 2; i < 12; i++)
+        {
+            u.pinMode(i, PinMode.Input_pullup);
+            Debug.Log("Pin " + i + " initiated.");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (u.digitalRead(2) == 0 || u.digitalRead(6) == 0)
+        {
+            transform.position += Vector3.left * speed * Time.deltaTime;
+        }
+        if (u.digitalRead(3) == 0 || u.digitalRead(7) == 0)
+        {
+            transform.position += Vector3.up * speed * Time.deltaTime;
+        }
+        if (u.digitalRead(4) == 0 || u.digitalRead(8) == 0)
+        {
+            transform.position += Vector3.down * speed * Time.deltaTime;
+        }
+        if (u.digitalRead(5) == 0 || u.digitalRead(9) == 0)
+        {
+            transform.position += Vector3.right * speed * Time.deltaTime;
+        }
+        /*if (u.digitalRead(10) == 0 || u.digitalRead(11) == 0)
+        {
+            isPaused = !isPaused;
+        }
+        if (isPaused)
+        {
+            Time.timeScale = 0;
+        }
+        else Time.timeScale = 1;*/
+
+    }
+
+}
---
+++ EGD_Project3/Assets/Movement_Temp.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Uduino;
+
+public class Movement_Temp : MonoBehaviour
+{
+    public int speed = 3;
+    public UduinoManager u;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        u = UduinoManager.Instance;
+        u.DiscoverPorts();
+        for (int i = 2; i < 6; i++)
+        {
+            u.pinMode(i, PinMode.Input_pullup);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (u.digitalRead(2) == 0)
+        {
+            transform.position += Vector3.left * speed * Time.deltaTime;
+        }
+        if (u.digitalRead(3) == 0)
+        {
+            transform.position += Vector3.up * speed * Time.deltaTime;
+        }
+        if (u.digitalRead(4) == 0)
+        {
+            transform.position += Vector3.down * speed * Time.deltaTime;
+        }
+        if (u.digitalRead(5) == 0)
+        {
+            transform.position += Vector3.right * speed * Time.deltaTime;
+        }
+    }
+}
